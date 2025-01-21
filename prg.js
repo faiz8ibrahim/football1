9c1eb3d275a8d04c0fa3c08281d5a2276e048f2a
Mohamed Amine Ouroui <166921147+Ourouimed@users.noreply.github.com>
2025-01-21 20:59:37 +0000

    Medamine sb3 salat khdmto hna hhhhh

--- prg.js
+++
@@ -1,4 +0,0 @@
-let header = document.querySelector('header')
-window.addEventListener('scroll' , ()=>{
-    window.scrollY > 80 ? header.classList.add('sticky') : header.classList.remove('sticky')
-})
